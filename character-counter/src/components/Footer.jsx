1f98c4dc6a2a2436761d729430963d3c2aa015f3
abdizahir <faarxgaloo226@gmail.com>
2025-09-10 14:58:04 +0300

    character-counter

--- character-counter/src/components/Footer.jsx
+++ character-counter/src/components/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
     const bgColor = darkMode ? 'text-natural-200' : 'text-natural-900';
     return (
         <div class={`text-center ${bgColor}`}>
-            Challenge by <a className="text-xl text-blue-500 hover:text-blue-700" href="https://x.com/abdalahmohamad" target="_blank">Frontend Mentor</a>. 
-            Coded by <a className="text-xl font-bold text-blue-500 hover:text-blue-700" href="https://web.facebook.com/Galool22">Abdalah</a>.
+            Challenge by <a className="text-xl text-blue-500 hover:text-blue-700" href="https://www.frontendmentor.io/challenges/character-counter-znSgeWs_i6" target="_blank">Frontend Mentor</a>. 
+            Coded by <a className="text-xl font-bold text-blue-500 hover:text-blue-700" href="https://x.com/abdalahmohamad" target='_blank'>Abdalah</a>.
         </div>
     );
 }
